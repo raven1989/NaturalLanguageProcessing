2816279f900cf9a754f906cd94df7eecfabe1c39
raven1989 <okuma0513@yahoo.com>
2017-07-13 01:26:34 +0800

    20170713@Rd.Beiyuan its too damn complicated

--- 0_Dictionary/Trie/DoubleArrayTrieDictionary.py
+++ 0_Dictionary/Trie/DoubleArrayTrieDictionary.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys, traceback
 import os
-import TrieDictionary.TrieNode as TrieNode
+from  TrieDictionary import TrieNode as TrieNode
 __pwd__ = os.path.dirname(os.path.realpath(__file__))
 
 class DoubleArrayTrieDictionary:
@@ -9,60 +9,81 @@ class DoubleArrayTrieDictionary:
     def __init__(self, base, check):
       self.base = base
       self.check = check
+    def __str__(self):
+      return str((self.base,self.check))
 
   def __init__(self):
     self.array = []
-  def __resize__(self, size, value=unit_t(0,0)):
+    self.default = self.unit_t(0,0)
+  def __resize__(self, size, default):
     if len(self.array)<size:
-      self.array.extend([value]*(size-len(self.array)))
+      self.array.extend([default]*(size-len(self.array)))
+      print [str(x) for x in self.array]
   def __locate_base__(self, s, node):
     cur_s = s
     while True:
       t = cur_s + node.siblings[0].code
-      if not cur_s+len(node.siblings[-1].code)<len(self.array):
-        self.__resize__(cur_s+len(node.siblings[-1].code+1+128))
+      if not cur_s+node.siblings[-1].code<len(self.array):
+        self.__resize__(cur_s+node.siblings[-1].code+1+128, self.default)
       i = 0
-      while i<len(node.siblings) and array[t].check==0:
+      while i<len(node.siblings) and self.array[t].check==0:
         i += 1
       else:
         if i==len(node.siblings):
-          return t
+          return cur_s
         else:
           i = 0
           cur_s += 1
-  def __generate_siblings__(self, node, words):
-    i = node.left
-    cnt = 0
-    while i<node.right:
-      word = word[i]
-      if len(word)<node.depth+1:
-        if len(word)==node.depth+1:
-          empty_node = TrieNode()
-          empty_node.code = 0
-          empty_node.depth = node.depth+1
-          empty_node.left = node.left
-          empty_node.right = node.left+1
-          node.siblings.append(empty_node)
-        else:
-          if word[pre.code!=word[i].code:
-            new_node = TrieNode()
-            new_node.code = ord(word[node.depth+1])+1
-            new_node.depth = node.depth+1
-            new_node.left = node.siblings[-1].right if len(node.siblings)>0 else node.left
-            new_node.right = node.siblings[-1].
-      pre = i
+  def __generate_siblings__(self, parent, words):
+    i = parent.left
+    pre = 0
+    while i<parent.right:
+      word = words[i]
+      if not len(word)<parent.depth:
+        cur = 0 if len(word)==parent.depth else ord(word[parent.depth])+1
+        if  len(parent.siblings)==0 or pre!=cur:
+          new_node = TrieNode()
+          new_node.code = cur
+          new_node.depth = parent.depth+1
+          new_node.left = i
+          if len(parent.siblings)>0:
+            parent.siblings[-1].right = i
+          parent.siblings.append(new_node)
+        pre = cur 
       i += 1
+    if len(parent.siblings)>0:
+      parent.siblings[-1].right = i
+    return len(parent.siblings)
+  def __insert_array__(self, s, root):
+    begin = s
+    base = self.__locate_base__(s, root)
+    print base
+    print len(self.array)
+    i = 0
+    while i<len(root.siblings):
+      print base+root.siblings[i].code
+      # print [str(x) for x in self.array]
+      self.array[base+root.siblings[i].code].check = begin
+      i+=1
+    self.array[begin].base = base
+    print [str(x) for x in self.array]
 
-  def build(self):
+  def build(self, dic):
     words = [x.strip().decode('utf-8') for x in open(__pwd__+'/../dict/'+dic)]
     words.sort()
+    self.__resize__(8192, self.default)
+    self.array[0].base = 1
+    self.array[1].check = 0
     root = TrieNode()
     root.code = 0
     root.depth = 0
     root.left = 0
     root.right = len(words)
+    siblings = self.__generate_siblings__(root, words)
+    print root
+    self.__insert_array__(2, root)
 
 
 if __name__ == '__main__':
-  print ord('一'.decode('utf-8'))
-  print ord('万'.decode('utf-8'))
+  da = DoubleArrayTrieDictionary()
+  da.build('test.dic')
